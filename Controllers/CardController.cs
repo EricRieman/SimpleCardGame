93a978a4bb4ccfee30b303fe8be80ed72046da5d
Eric Rieman <er28@zips.uakron.edu>
2021-03-11 00:49:03 -0500

    Playing around with rotating the card.

--- Controllers/CardController.cs
+++ Controllers/CardController.cs
@@ -2,15 +2,34 @@
 
 public class CardController : MonoBehaviour
 {
-   [SerializeField] private GameObject CardBack;
+   public Quaternion start;
+   public float lerpTime = 1;
 
-   private void OnMouseDown()
+//   private void OnMouseDown()
+//   {
+//      FlipCard();
+//   }
+//
+//   public void FlipCard()
+//   {
+//      //CardBack.SetActive( !CardBack.activeSelf );
+//
+//      for ( float i = 0; i <=180; i+= 10 )
+//      {
+//         transform.rotation = Quaternion.Euler( 0, i, 0 );
+//      }
+//
+//
+//   }
+
+   void Start()
    {
-      FlipCard();
+      start = transform.rotation;
    }
 
-   public void FlipCard()
+   void Update()
    {
-      CardBack.SetActive( !CardBack.activeSelf );
+      //transform.rotation = Quaternion.Lerp( transform.rotation, start, Time.deltaTime * lerpTime );
+      transform.Rotate( Vector3.up );
    }
 }
